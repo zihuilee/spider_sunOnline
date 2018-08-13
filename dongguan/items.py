9e1d02badd621350c8d8832193cee782d7fd6bb4
zihuilee <lizihui126126@126.com>
2018-08-13 16:40:02 +0800

    first commit

---
+++ dongguan/items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DongguanItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    # 每个帖子的标题
+    title = scrapy.Field()
+    # 每个帖子的编号
+    number = scrapy.Field()
+    # 每个帖子的文字内容
+    content = scrapy.Field()
+    # 每个帖子的url
+    url = scrapy.Field()
